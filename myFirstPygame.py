0ae0af0ab58b817734c84ffc7dabab77cfe85139
Bambi R <s11442985@students.duvalschools.org>
2021-11-30 13:08:31 -0500

    v0.2 [7b]
    initialized pygame and set up windows to draw on

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,5 +1,10 @@
-# my first PyGame, Bambi Romedy, 11/30/21, 12:14pm, v0.1
+# my first PyGame, Bambi Romedy, 11/30/21, 12:14pm, v0.2
 
 import pygame, sys
 from pygame.locals import *
 
+# Initialize PyGame
+pygame.init()
+
+# set up the window to draw on.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
