53fb3c4cbfca193f9bff02be41f68bb1d65f83b4
Bambi <s11442985@students.duvalschools.org>
2022-01-04 13:18:18 -0500

    v0.1 [7b]
    setup pygame and the window

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animimation with Pygame, Bambi Romedy, 01/04/22, 1:11pm, v0.0
+# Simple Animimation with Pygame, Bambi Romedy, 01/04/22, 1:17pm, v0.1
+
+import pygame, sys, time 
+from pygame.locals import
+
+# Setup PyGame
+pygame.init()
+
+# Setup the Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowsurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
